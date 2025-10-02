06e57016bf2381834e6c8a90f72adb4a81bf8f59
Noah Normand <noah.normand@student.junia.com>
2025-10-02 09:51:10 +0200

    fruit done

---
+++ src/main/java/com/jad/Fruit.java
@@ -0,0 +1,13 @@
+package com.jad;
+
+public class Fruit {
+    public Fruit(String name) {
+        this.name = name;
+    }
+
+    private final String name;
+
+    public String getName() {
+        return name;
+    }
+}
